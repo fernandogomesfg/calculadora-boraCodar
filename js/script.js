cce32f9596a0f6ca67965109e241d9cbfbe12708
Fernando Gomes <fernandogomesbernardo@gmail.com>
2023-02-08 13:38:09 -0800

    chore: varias alteracoes
    Alteracao da ordem dos numeros e operadores no teclado
    Verificacao dos botoes para ver se eh um numero ou operador
    Estilizacao dos botoes dos numeros

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
 const teclado = document.getElementById("teclado");
-const buttons = ['CE','C','%', '/', 7, 8, 9,'X',4,5,6, '-',1,2,3, '+','+/-', 0,',','='];
+const buttons = ['CE', 'C', '%', '/', 7, 8, 9, 'X', 4, 5, 6, '-', 1, 2, 3, '+', '+/-', 0, ',', '='];
 
 for (const button of buttons) {
     const btn = document.createElement("button");
     btn.innerText = button;
+
+    // verificando se o botao eh um numero ou nao 
+    // e atriuir uma classe especifica para estilizar
+    if (typeof button === 'number') {
+        btn.classList.add("number");
+    } else {
+        btn.classList.add("operator");
+    }
+
     teclado.appendChild(btn);
 
     if (buttons.indexOf(button) % 4 === 3) {
